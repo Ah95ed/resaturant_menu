ef61f1b7f9394d1489c3911bf1525d2ba7d791ac
Ahmed Shaker <amhmeed31@gmail.com>
2025-02-07 00:31:36 +0300

    remove file output apk

--- lib/Models/CartModels.dart
+++ lib/Models/CartModels.dart
@@ -1,9 +1,13 @@
 class Cartmodels {
+
   late String name;
+
   late double price;
+
   late int quantity;
 
   Cartmodels({
+    
     required this.name,
     required this.price,
     required this.quantity,
